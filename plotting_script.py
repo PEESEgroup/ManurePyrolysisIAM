1630c38866d284a27f418eebaa43332736d7de47
nathan-preuss <nnp25@cornell.edu>
2025-11-24 16:54:39 -0500

    update CI interval code

--- plotting_script.py
+++ plotting_script.py
@@ -902,7 +902,7 @@ def main():
                       "LowAdoption30",
                       "LowCarbonPersistence"]
     biochar_year = "2050"
-    figure1(other_scenario, reference_RCP, reference_SSP, biochar_year)
+    # figure1(other_scenario, reference_RCP, reference_SSP, biochar_year)
     figure2(other_scenario, reference_RCP, reference_SSP, biochar_year)
     figure3(other_scenario, reference_RCP, reference_SSP, biochar_year)
     figure4(other_scenario, reference_RCP, reference_SSP, biochar_year)
